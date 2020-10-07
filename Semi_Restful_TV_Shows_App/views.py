a87199490a87531c1e75633f9eccd7437137024d
Imran <imran_khan2888@hotmail.com>
2020-10-06 19:37:05 -0500

    validation for edit

--- Semi_Restful_TV_Shows_App/views.py
+++ Semi_Restful_TV_Shows_App/views.py
@@ -43,23 +43,30 @@ def display_show(request, show_id):
     
 
 def edit(request, show_id):
-
     context = {
     'show':  Show.objects.get(id=show_id)
     }
     return render(request,'edit.html',context)
 
 def update(request, show_id):
+    context = {
+    'show':  Show.objects.get(id=show_id)
+    }
+    errors = Show.objects.validate(request.POST)
+    if errors:
+        for (key, value) in errors.items():
+            messages.error(request, value)
+        return render(request,'edit.html',context)
     # update show!
     to_update = Show.objects.get(id=show_id)
     # updates each field
     to_update.title = request.POST['title']
-    to_update.release_date = request.POST['releasedate']
+    to_update.release_date = request.POST['release_date']
     to_update.network = request.POST['network']
     to_update.description = request.POST['description']
     to_update.save()
 
-    return redirect('/shows/')
+    return redirect('/shows')
 
 
 def delete(request, show_id):
